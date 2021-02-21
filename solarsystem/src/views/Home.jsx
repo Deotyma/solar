5968b452294c3754f549f1c4f0b519fabd95b54d
Ewa <kalistaewa@gmail.com>
2021-02-21 03:05:18 +0100

    serch bar and css

--- solarsystem/src/views/Home.jsx
+++ solarsystem/src/views/Home.jsx
@@ -5,7 +5,34 @@ class Home extends React.Component {
     return (
       <div className="">
         <div className="container">
-          <h1 className="text-center">Solar System</h1>
+          <h1 className="text-center m-5">Our Solar System</h1>
+
+          <div className=" row d-flex justify-content-center col-12">
+            <label
+              for="exampleDataList"
+              className="form-label col-12 text-center m"
+            >
+              <h3>Your research</h3>
+            </label>
+            <input
+              class="form-control col"
+              list="datalistOptions"
+              id="exampleDataList"
+              placeholder="Type to search..."
+            ></input>
+            <button
+              className="btn button btn-light button col-lg-3 col-md-3 col-sm-4"
+              type="button"
+              id="inputGroupFileAddon04"
+            >
+              Submit
+            </button>
+          </div>
+          <p className="paragraphe mt-5 text-center">
+            Welcome to my page about the Solar System. Here you will find
+            informations about the planets and their natural satellites as well
+            as pictures of some of them. Have a good fun.
+          </p>
         </div>
       </div>
     );
--- solarsystem/src/views/Planets.jsx
+++ solarsystem/src/views/Planets.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "./Planets.css";
 
 class Planets extends React.Component {
   render() {
     return (
-      <div className="container">
-        <h1>This is Planets Page</h1>
+      <div className="planets">
+        <div className="container">
+          <h1>This is Planets Page</h1>
+        </div>
       </div>
     );
   }
